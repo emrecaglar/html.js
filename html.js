b5582ead01c95ab0a5513a9858a7f1eba5fb936a
emrecaglar <emre52@gmail.com>
2018-11-14 11:00:52 -0800

    refactor and add new html elements

--- html.js
+++ html.js
@@ -1,113 +1,268 @@
-var html = (function(){
-    function setClassAttribute(element, key, value){
-        if(typeof value == 'object' && value instanceof Array){
-            element.className = value.join(' ');
-        }else {
-            element.className = value;
+var html = (function () {
+    function getVariableType(variable) {
+        if (typeof variable == 'object') {
+            if (variable instanceof Function) {
+                return 'function';
+            } else if (variable instanceof Array) {
+                return 'array';
+            } else if (variable instanceof HTMLElement) {
+                return 'html'
+            }
+
+            return 'object';
+        }
+
+        return typeof variable;
+    }
+
+    function attributeSetter(attributeType) {
+        var attributeHandlers = {
+            'class': function (element, key, value) {
+                var type = getVariableType(value);
+
+                if (type == 'array') {
+                    element.className = value.join(' ');
+                } else {
+                    element.className = value;
+                }
+            },
+            'style': function (element, key, value) {
+                var type = getVariableType(value);
+
+                if (type == 'object') {
+                    for (var key in value) {
+                        element.style.cssText += key + ':' + value[key] + ';';
+                    }
+                } else {
+                    element.cssText = value;
+                }
+            }
+        };
+
+        return attributeHandlers[attributeType] || function (element, key, value) {
+            element.setAttribute(key, value);
         }
     }
 
-    function setStyleAttribute(element,key, value){
-        if(typeof value == 'object'){
-            for(var key in value){
-                element.style.cssText += key+':'+value[key]+';';
-            }
-        }else {
-            element.cssText = value;
-        }
-    }
+    function setAttributes(element, args) {
+        if (args.length > 0) {
+            var type = getVariableType(args[0]);
 
-    function setDefaultAttribute(element, key, value){
-        element.setAttribute(key, value);
-    }
+            if(type == 'object'){
+                var attributes = args[0];
 
-    function setAttributes(element, attributes){
-        for(var key in attributes){
-            switch(key){
-                case 'class':
-                    setClassAttribute(element,key, attributes[key]);
-                    break;
-                case 'style':
-                    setStyleAttribute(element, key, attributes[key]);
-                    break;
-                default:
-                    setDefaultAttribute(element, key, attributes[key]);
-                    break;
+                for (var key in attributes) {
+                    attributeSetter(key)(element, key, attributes[key]);
+                }
             }
         }
     }
 
-    function elementParameterHandler(element, args){
-        if(args.length == 1){
-            var type = typeof args[0];
+    function setContent(element, args) {
+        if (args.length > 0) {
+            var type = getVariableType(args[0]);
 
-            if(type == 'object' && args[0] instanceof Function){
-                
-                    element.innerHTML = args[0]();
-            }
-             else if(type =='string'){
-                    element.innerHTML = args[0];
-             }
-              else{
-                    setAttributes(element, args[0]);
-            }
-        }else{
-            var startIndex = 1;
+            var startindex = type == 'object' ? 1 : 0;
 
-            if(typeof args[0] == 'object'){
-                setAttributes(element, args[0]);
-            }else{
-                startIndex = 0;
-            }
-
-            
-            for(var i = startIndex;i<args.length;i++){
-                var type = typeof args[i];
-
-                if(type == 'object'){
-                    
+            for (var i = startindex; i < args.length; i++) {
+                var type = getVariableType(args[i]);
+    
+                switch (type) {
+                    case 'html':
                         element.appendChild(args[i]);
-                }
-                 else if(type == 'string'){
+                        break;
+                    default:
                         element.innerHTML += args[i];
+                        break;
                 }
             }
         }
     }
 
-    function Div(){
-        var element = document.createElement('div');
+    function generateElement(type, args) {
+        var element = document.createElement(type);
 
-        var args = arguments;
+        setAttributes(element, args);
 
-        elementParameterHandler(element, args);
-
-        return element;
-    }
-
-    function P(){
-        var element = document.createElement('p');
-
-        var args = arguments;
-
-        elementParameterHandler(element, args);
-
-        return element;
-    }
-
-    function Strong(){
-        var element = document.createElement('strong');
-
-        var args = arguments;
-
-        elementParameterHandler(element, args);
+        setContent(element, args);
 
         return element;
     }
 
     return {
-        Div: Div,
-        P:P,
-        Strong: Strong
+        Div: function () {
+            return generateElement('div', arguments);
+        },
+        Paragraph: function(){
+            return generateElement('p', arguments);
+        },
+        Strong: function(){
+            return generateElement('strong', arguments);
+        },
+        Table: function(){
+            return generateElement('table', arguments);
+        },
+        THead: function(){
+            return generateElement('thead', arguments);
+        },
+        TBody: function(){
+            return generateElement('tbody', arguments);
+        },
+        Tr: function(){
+            return generateElement('tr', arguments);
+        },
+        Td: function(){
+            return generateElement('td', arguments);
+        },
+        Th: function(){
+            return generateElement('th', arguments);
+        },
+        IFrame: function(){
+            return generateElement('iframe', arguments);
+        },
+        Ul: function(){
+            return generateElement('ul', arguments);
+        },
+        Li: function(){
+            return generateElement('li', arguments);
+        },
+        Anchor: function(){
+            return generateElement('a', arguments);
+        },
+        Br: function(){
+            return generateElement('br', arguments);
+        },
+        Hr: function(){
+            return generateElement('hr', arguments);
+        },
+        Select: function(){
+            return generateElement('select', arguments);
+        },
+        Option: function(){
+            return generateElement('option', arguments);
+        },
+        Input: function(){
+            return generateElement('input', arguments);
+        },
+        TextArea: function(){
+            return generateElement('textarea', arguments);
+        },
+        Checkbox: function(){
+            return generateElement('checkbox', arguments);
+        },
+        Address: function(){
+            return generateElement('address', arguments);
+        },
+        Area: function(){
+            return generateElement('area', arguments);
+        },
+        B: function(){
+            return generateElement('b', arguments);
+        },
+        Aside: function(){
+            return generateElement('aside', arguments);
+        },
+        Article: function(){
+            return generateElement('article', arguments);
+        },
+        Audio: function(){
+            return generateElement('audio', arguments);
+        },
+        Button: function(){
+            return generateElement('button', arguments);
+        },
+        Caption: function(){
+            return generateElement('caption', arguments);
+        },
+        Cite: function(){
+            return generateElement('cite', arguments);
+        },
+        Code: function(){
+            return generateElement('code', arguments);
+        },
+        Col: function(){
+            return generateElement('col', arguments);
+        },
+        ColGroup: function(){
+            return generateElement('colgroup', arguments);
+        },
+        Dd: function(){
+            return generateElement('dd', arguments);
+        },
+        Del: function(){
+            return generateElement('del', arguments);
+        },
+        Dl: function(){
+            return generateElement('dl', arguments);
+        },
+        Dt: function(){
+            return generateElement('dt', arguments);
+        },
+        Em: function(){
+            return generateElement('em', arguments);
+        },
+        Fieldset: function(){
+            return generateElement('fieldset', arguments);
+        },
+        Legend: function(){
+            return generateElement('legend', arguments);
+        },
+        Form: function(){
+            return generateElement('form', arguments);
+        },
+        H1: function(){
+            return generateElement('h1', arguments);
+        },
+        H2: function(){
+            return generateElement('h2', arguments);
+        },
+        H3: function(){
+            return generateElement('h3', arguments);
+        },
+        H4: function(){
+            return generateElement('h4', arguments);
+        },
+        H5: function(){
+            return generateElement('h5', arguments);
+        },
+        H6: function(){
+            return generateElement('h6', arguments);
+        },
+        Header: function(){
+            return generateElement('header', arguments);
+        },
+        Footer: function(){
+            return generateElement('fooeter', arguments);
+        },
+        I: function(){
+            return generateElement('i', arguments);
+        },
+        Image: function(){
+            return generateElement('img', arguments);
+        },
+        Label: function(){
+            return generateElement('label', arguments);
+        },
+        Nav: function(){
+            return generateElement('nav', arguments);
+        },
+        Pre: function(){
+            return generateElement('pre', arguments);
+        },
+        Small: function(){
+            return generateElement('small', arguments);
+        },
+        Video: function(){
+            return generateElement('video', arguments);
+        },
+        Svg: function(){
+            return generateElement('svg', arguments);
+        },
+        Span: function(){
+            return generateElement('span', arguments);
+        },
+        OptionGroup: function(){
+            return generateElement('optgroup', arguments);
+        }
     }
 })();
